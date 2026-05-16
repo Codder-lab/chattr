4f7a38b80b8dedca705a2c9b93c297f4132594a7
suyash-potdar <suyash.potdar@alphawarenext.com>
2026-05-16 15:33:59 +0530

    feat: implement chat, message, and user controllers with corresponding protected routes

--- backend/src/routes/chat.route.ts
+++ backend/src/routes/chat.route.ts
@@ -1,5 +1,17 @@
 import { Router } from "express";
+import { protectRoute } from "../middleware/auth";
+import { getChats, getOrCreateChat } from "../controllers/chat.controller";
 
 const router = Router();
+
+// @route GET /api/chat/
+// @desc Get all chats for current user
+// @access Private
+router.get("/", protectRoute, getChats);
+
+// @route POST api/chat/with/:participantId
+// @desc Get or create chat with another user
+// @access Private
+router.post("/with/:participantId", protectRoute, getOrCreateChat);
 
 export default router;
--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import { protectRoute } from "../middleware/auth";
+import { getMessages } from "../controllers/message.controller";
 
 const router = Router();
+
+// @route: GET /api/message/chat/:chatId
+// @desc: Get all messages for a specific chat
+// @access: Protected
+router.get("/chat/:chatId", protectRoute, getMessages);
 
 export default router;
